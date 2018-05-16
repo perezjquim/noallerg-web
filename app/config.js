556cc02afbbc7a8b141d87f0563d0cad855a65d0
Joaquim Perez <jquim1@hotmail.com>
2018-05-16 01:25:25 +0100

    Cleanup

--- app/config.js
+++
@@ -1,9 +0,0 @@
-//UI5 Boilerplate Configuration
-jQuery.sap.declare("ui5bp.app.config");
-
-ui5bp.app.config = {
-
-	//Navigation Mode (LaunchpadMode: false -> LeftMenu Navigation)
-    LaunchpadMode: true
-
-};
